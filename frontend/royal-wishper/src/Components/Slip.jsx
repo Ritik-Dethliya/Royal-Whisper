5955867d8a2054a058a3780064addee012df6074
Ritik-Dethliya <dethliya73@gmail.com>
2025-06-09 11:16:10 +0530

    handel select

--- frontend/royal-wishper/src/Components/Slip.jsx
+++ frontend/royal-wishper/src/Components/Slip.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../Styles/slip.css'
+import { io } from "socket.io-client";
 function Slips(){
     let [isthrow,setIsThrow]=useState(true)
+    const [selectedSlips,setSelectedSlips]=useState([])
+    const socket=io('http://localhost:8000/')
+    useEffect(()=>{
+        socket.emit('join-room',123)
+    },[])
+    const selectSlip=(slipId)=>{
+        try {
+            const roomId=123
+            if(selectedSlips.includes(slipId))return alert('already selected')
+            else{
+                socket.emit('select-slip',{roomId,slipId})
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+    socket.on('slip-selected',({selecter,slipId})=>{
+        alert(`${selecter} select splip number,${slipId}`)
+        setSelectedSlips((prev)=>[...prev,slipId])
+    })
     return (  
         <>
         <div className="slip-container">
             {
                 isthrow &&
                 <>
-                    <div className="slips fly fly1">slip1</div>
-                    <div className="slips fly fly2">slip2</div>
-                    <div className="slips fly fly3">slip3</div>
-                    <div className="slips fly fly4">slip4</div>
+                    <button className="slips fly fly1" id={101}  onClick={()=>selectSlip(101)}>slip1</button>
+                    <button className="slips fly fly2" id={201} onClick={()=>selectSlip(201)}>slip2</button>
+                    <button className="slips fly fly3" id={301} onClick={()=>selectSlip(301)}>slip3</button>
+                    <button className="slips fly fly4" id={401} onClick={()=>selectSlip(401)}>slip4</button>
                 </>
             }
            
